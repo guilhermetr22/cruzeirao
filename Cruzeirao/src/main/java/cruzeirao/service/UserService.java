7067b77748d63fc5e82f3935f1181e7d005e9729
guilh <guilh@Guilherme-PC>
2018-03-20 19:50:18 -0300

    Criando menu e telas

--- Cruzeirao/src/main/java/cruzeirao/service/UserService.java
+++ Cruzeirao/src/main/java/cruzeirao/service/UserService.java
@@ -3,23 +3,41 @@ package cruzeirao.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 import cruzeirao.modelos.User;
 
 public class UserService {
-	private ArrayList <User> usuarios = new ArrayList<>();
+	
+	private EntityManagerFactory emf;
 	
 	public UserService()
 	{
-		
+		emf = Persistence.createEntityManagerFactory("Cruzeirao");
 	}
 	
 	public void salvar(User usuario)
 	{
-		usuarios.add(usuario);
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();	
+			em.persist(usuario);
+		em.getTransaction().commit();	
+	    em.close();
 	}
 	
+	@SuppressWarnings("unchecked")
 	public List<User> getUsuarios()
 	{
+		List <User> usuarios;
+		
+		EntityManager em = emf.createEntityManager();
+		Query q = em.createQuery("Select a From Aluno a");
+		usuarios = q.getResultList();
+		em.close();
+		
 		return usuarios;
 	}
 }
